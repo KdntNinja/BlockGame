7c879b4bfc43c8924fcbe2873473f97817554bad
KdntNinja <kaidentninja@gmail.com>
2024-02-13 20:30:26 +0000

    cloud improvements

--- Voxel_Engine/shaders/clouds.frag
+++ Voxel_Engine/shaders/clouds.frag
@@ -2,12 +2,14 @@
 
 layout (location = 0) out vec4 fragColor;
 
-const vec3 cloud_color = vec3(1);
-
 uniform vec3 bg_color;
+uniform float u_time;
 
 void main() {
     float fog_dist = gl_FragCoord.z / gl_FragCoord.w;
+
+    vec3 cloud_color = vec3(0.5 * sin(0.1 * u_time) + 0.5);
+
     vec3 col = mix(cloud_color, bg_color, 1.0 - exp(-0.000001 * fog_dist * fog_dist));
 
     fragColor = vec4(col, 0.8);
--- Voxel_Engine/world_objects/clouds.py
+++ Voxel_Engine/world_objects/clouds.py
@@ -5,9 +5,13 @@ class Clouds:
     def __init__(self, app):
         self.app = app
         self.mesh = CloudMesh(app)
+        self.speed = 0.01
 
     def update(self):
         self.mesh.program["u_time"] = self.app.time
+        current_center = self.mesh.program["center"].value
+        new_center = current_center + self.speed
+        self.mesh.program["center"].value = int(new_center)
 
     def render(self):
         self.mesh.render()
